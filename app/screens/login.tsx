44afbbb3befdc76baa0f4b9a44e2b4d950a3a85b
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-11-18 09:30:38 +0000

    login screen

---
+++ app/screens/login.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginForm from '../forms/login/login-form'
+
+const Login = () => {
+    return (
+        <View>
+            <LoginForm />
+        </View>
+    )
+}
+
+export default Login
