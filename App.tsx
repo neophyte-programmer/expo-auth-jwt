66026d75ade2cab6568550ebe8603da5227f0f67
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-11-18 09:31:42 +0000

    structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthProvider } from './app/context/auth-context';
+import { NavigationLayout } from './app/layout/navigation-layout';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <NavigationLayout>
+        
+      </NavigationLayout>
+    </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
